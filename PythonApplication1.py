8aa4c97c30435eb269ddd1b0c7b596921664e4b1
ChiehYu Shih <jj830502@gmail.com>
2019-10-11 21:36:25 +0800

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -2,13 +2,16 @@
 
 #Lesson1 Variables Testing
 
-price = 80
-print ( price )
+price_1 = 80                                                                        #數值變數(整數)
+price_2 = 80.0                                                                      #樹值變數(浮點數)
+print ( price_1,price_2 )
 
-fruit = "水果"
-print ( fruit )
+fruit = "水果"                                                                      #字串變數 
+print ( fruit )                                                                     #打印括弧內東西  
+print (fruit,"價格等於",price_1)                                                    #雙引號可以直接打印引號內容
 
-print (fruit,"價格等於",price)
-
-
+price_1 = price_2 = 60.0                                                            #變數連續定義(以等號表示相同且意義也相同之變數)
+print (price_1,price_2)
+price_3,car = 120,"汽車"                                                            #變數練續定義(不同變數以逗號分別)
+print (car,"價格為",price_3)
 
