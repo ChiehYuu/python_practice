95bbbff679e2ab22c1eae35639741a3f5ed67253
ChiehYu Shih <jj830502@gmail.com>
2019-11-08 22:37:24 +0800

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -458,9 +458,3 @@ func_grade(1,2,3,4,5,6,7,8,9)
 ##############################################################################################
 ##############################################################################################
 
-import ast
-data = dict()
-with open('password.txt','r', encoding = 'UTF-8-sig') as f:
-   filedata = f.read()
-   data = ast.literal_eval(filedata)
-print(type(data),data)
